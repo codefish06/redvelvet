8c3ef110630c70ab891ff401c5e98bd65dde3fd7
Gerald Tecson <gtecson@pacificseainvests.com>
2017-04-14 16:09:09 +0800

    Edit function added.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+    # Validates :title, presence: true,
+    #                     length: { minimum: 5 }
+    validates :title, presence: true,
+              length: { minimum: 5 }
 end
